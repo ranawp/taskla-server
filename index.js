ec08ec9ee1aa812a072fe7595823841918492419
MASUD-01 <masudrana96141@gmail.com>
2022-08-02 21:27:42 +0600

    user profile

--- index.js
+++ index.js
@@ -25,6 +25,12 @@ async function run() {
             const users = await userCollection.find().toArray()
             res.send(users)
         })
+        app.get('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const users = await userCollection.findOne(filter)
+            res.send(users)
+        })
 
         app.put('/user/admin/:email', async (req, res) => {
             const email = req.params.email;
@@ -35,18 +41,45 @@ async function run() {
             const result = await userCollection.updateOne(filter, updateDoc);
             res.send(result);
         })
+        app.put('/user/student/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { student: 'paidStudent' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
+        })
+        app.put('/update/:email', async (req, res) => {
+            const email = req.params.email;
+            console.log(email)
             const user = req.body;
             const filter = { email: email };
             const options = { upsert: true };
             const updateDoc = {
                 $set: user,
             };
-            const result = await userCollection.updateOne(filter, updateDoc, options);
-            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-            res.send({ result, token });
+            console.log(user)
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.post('/user', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user)
+            res.send(result)
         })
 
     }
